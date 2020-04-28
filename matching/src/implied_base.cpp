79b816d14ed6f6d0a391486686fb86e3a4e49a0a
David AU <kingbirdogd@gmail.com>
2020-04-29 00:47:52 +0800

    add engine_type

--- matching/src/implied_base.cpp
+++ matching/src/implied_base.cpp
@@ -6,6 +6,7 @@ using namespace matching;
 void implied_base::_place_order(engine* e, implied_order& o)
 {
 	o.implied_ptr = this;
+	o.engine_type = order::order_engine_type::IMPLIED;
 	e->_handle(o);
 }
 
