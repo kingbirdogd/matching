4f2da48536d1dcf225dc2da3c6a9816e65e0c93d
David AU <kingbirdogd@gmail.com>
2020-05-02 16:35:51 +0800

    add base spin mutex

---
+++ core/inc/spin_mutex.hpp
@@ -0,0 +1,48 @@
+#ifndef CORE_INC_SPIN_MUTEX_HPP_
+#define CORE_INC_SPIN_MUTEX_HPP_
+#include <atomic>
+
+namespace core
+{
+	class spin_mutex
+	{
+	private:
+		using flag = std::atomic<unsigned long long>;
+	private:
+		flag _f;
+	public:
+		spin_mutex():
+			_f(0)
+		{
+		}
+		~spin_mutex() = default;
+		spin_mutex(const spin_mutex&) = delete;
+		spin_mutex(spin_mutex&&) = delete;
+		spin_mutex& operator= (const spin_mutex&) = delete;
+		spin_mutex& operator= (spin_mutex&&) = delete;
+		void lock()
+		{
+			unsigned long long expected = 0;
+			do
+			{
+				expected = 1;
+			}
+			while (!_f.compare_exchange_weak(expected, 1, std::memory_order_relaxed, std::memory_order_relaxed));
+		}
+
+		bool try_lock()
+		{
+			unsigned long long expected = 0;
+			return _f.compare_exchange_weak(expected, 1, std::memory_order_relaxed, std::memory_order_relaxed);
+		}
+
+		void unlock()
+		{
+			_f.store(0, std::memory_order_relaxed);
+		}
+	};
+}
+
+
+
+#endif /* CORE_INC_SPIN_MUTEX_HPP_ */
