13e311300147089353fe1d3208e58c91059b8e0e
Peter Chan <peter.chan@coinflex.com>
2020-05-14 15:10:35 +0800

    fixed CANCEL

--- proxy_ws/Uplink.cpp
+++ proxy_ws/Uplink.cpp
@@ -10,152 +10,156 @@ std::unordered_map<unsigned long long, unsigned long long> client_to_engine_id_m
 
 json::Object handle_order(const matching::order& o)
 {
-  std::string type = "";
-  std::string side = "";
-  std::string status = "";
-  std::string time_condition = "";
-  std::string action = "";
-  std::string matched_type = "";
-  if (matching::order::order_type::LIMITED == o.type)
-  {
-    type = "LIMITED";
-  }
-  else
-  {
-    type = "MARKET";
-  }
-  if (matching::order::order_side::BUY == o.side)
-  {
-    side = "BUY";
-  }
-  else if (matching::order::order_side::SELL == o.side)
-  {
-    side = "SELL";
-  }
-  else if (matching::order::order_side::BUY_STOP == o.side)
-  {
-    side = "BUY_STOP";
-  }
-  else if (matching::order::order_side::SELL_STOP == o.side)
-  {
-    side = "SELL_STOP";
-  }
-  else if (matching::order::order_side::BUY_SELL_STOP == o.side)
-  {
-    side = "BUY_SELL_STOP";
-  }
-  else
-  {
-    side = "SELL_BUY_STOP";
-  }
-  if (o.order_state == matching::order::order_status_type::OPEN)
-  {
-    status = "OPEN";
-  }
-  else if (o.order_state == matching::order::order_status_type::PARTIAL_FILL)
-  {
-    status = "PARTIAL_FILL";
-  }
-  else if (o.order_state == matching::order::order_status_type::FILLED)
-  {
-    status = "FILLED";
-  }
-  else if (o.order_state == matching::order::order_status_type::CANCELED_BY_MARKET_ORDER_NOT_FULL_MATCHED)
-  {
-    status = "CANCELED_BY_MARKET_ORDER_NOT_FULL_MATCHED";
-  }
-  else if (o.order_state == matching::order::order_status_type::CANCELED_BY_MARKET_ORDER_NOTHING_MATCH)
-  {
-    status = "CANCELED_BY_MARKET_ORDER_NOTHING_MATCH";
-  }
-  else if (o.order_state == matching::order::order_status_type::CANCELED_ALL_BY_IOC)
-  {
-    status = "CANCELED_ALL_BY_IOC";
-  }
-  else if (o.order_state == matching::order::order_status_type::CANCELED_PARTIAL_BY_IOC)
-  {
-    status = "CANCELED_PARTIAL_BY_IOC";
-  }
-  else if (o.order_state == matching::order::order_status_type::CANCELED_BY_FOK)
-  {
-    status = "CANCELED_BY_FOK";
-  }
-  else if (o.order_state == matching::order::order_status_type::CANCELED_BY_MAKER_ONLY)
-  {
-    status = "CANCELED_BY_MAKER_ONLY";
-  }
-  else if (o.order_state == matching::order::order_status_type::REJECT_CANCEL_ORDER_ID_NOT_FOUND)
-  {
-    status = "REJECT_CANCEL_ORDER_ID_NOT_FOUND";
-  }
-  else if (o.order_state == matching::order::order_status_type::REJECT_AMEND_ORDER_ID_NOT_FOUND)
-  {
-    status = "REJECT_AMEND_ORDER_ID_NOT_FOUND";
-  }
-  else if (o.order_state == matching::order::order_status_type::REJECT_DISPLAY_QUANTITY_LARGER_THAN_QUANTITY)
-  {
-    status = "REJECT_DISPLAY_QUANTITY_LARGER_THAN_QUANTITY";
-  }
-  else if (o.order_state == matching::order::order_status_type::REJECT_BUY_STOP_TRIGGER_LARGE_THAN_STOP_LIMITED)
-  {
-    status = "REJECT_BUY_STOP_TRIGGER_LESS_THAN_STOP_LIMITED";
-  }
-  else if (o.order_state == matching::order::order_status_type::REJECT_SELL_STOP_TRIGGER_LESS_THAN_STOP_LIMITED)
-  {
-    status = "REJECT_SELL_STOP_TRIGGER_LESS_THAN_STOP_LIMITED";
-  }
-  else if (o.order_state == matching::order::order_status_type::REJECT_UNKNOW_ORDER_ACTION)
-  {
-    status = "REJECT_UNKNOW_ORDER_ACTION";
-  }
-  else if (o.order_state == matching::order::order_status_type::REJECT_QUANTITY_ZERO)
-  {
-    status = "REJECT_QUANTITY_ZERO";
-  }
-  else
-  {
-    status = "REJECT_LIMITE_ORDER_WITH_MARKET_PRICE";
-  }
-  if (matching::order::order_time_condition::GTC == o.time_condition)
-  {
-    time_condition = "GTC";
-  }
-  else if (matching::order::order_time_condition::IOC == o.time_condition)
-  {
-    time_condition = "IOC";
-  }
-  else if (matching::order::order_time_condition::FOK == o.time_condition)
-  {
-    time_condition = "FOK";
-  }
-  else if (matching::order::order_time_condition::MAKER_ONLY == o.time_condition)
-  {
-    time_condition = "MAKER_ONLY";
-  }
-  else
-  {
-    time_condition = "MAKER_ONLY_REPRICE";
-  }
-  if (matching::order::order_action_type::NEW == o.order_action)
-  {
-    action = "NEW";
-  }
-  else if (matching::order::order_action_type::CANCEL == o.order_action)
-  {
-    action = "CANCEL";
-  }
-  else
-  {
-    action = "AMEND";
-  }
-  if (matching::order::order_matched_type::TAKER == o.matched_type)
-  {
-    matched_type = "TAKER";
-  }
-  else
-  {
-    matched_type = "MAKER";
-  }
+    std::string type = "";
+    std::string side = "";
+    std::string status = "";
+    std::string time_condition = "";
+    std::string action = "";
+    std::string matched_type = "";
+    if (matching::order::order_type::LIMITED == o.type)
+    {
+      type = "LIMITED";
+    }
+    else
+    {
+      type = "MARKET";
+    }
+    if (matching::order::order_side::BUY == o.side)
+    {
+      side = "BUY";
+    }
+    else if (matching::order::order_side::SELL == o.side)
+    {
+      side = "SELL";
+    }
+    else if (matching::order::order_side::BUY_STOP == o.side)
+    {
+      side = "BUY_STOP";
+    }
+    else if (matching::order::order_side::SELL_STOP == o.side)
+    {
+      side = "SELL_STOP";
+    }
+    else if (matching::order::order_side::BUY_SELL_STOP == o.side)
+    {
+      side = "BUY_SELL_STOP";
+    }
+    else
+    {
+      side = "SELL_BUY_STOP";
+    }
+    if (o.order_state == matching::order::order_status_type::OPEN)
+    {
+      status = "OPEN";
+    }
+    else if (o.order_state == matching::order::order_status_type::PARTIAL_FILL)
+    {
+      status = "PARTIAL_FILL";
+    }
+    else if (o.order_state == matching::order::order_status_type::FILLED)
+    {
+      status = "FILLED";
+    }
+    else if (o.order_state == matching::order::order_status_type::CANCELED_BY_USER)
+    {
+      status = "CANCELED_BY_USER";
+    }
+    else if (o.order_state == matching::order::order_status_type::CANCELED_BY_MARKET_ORDER_NOT_FULL_MATCHED)
+    {
+      status = "CANCELED_BY_MARKET_ORDER_NOT_FULL_MATCHED";
+    }
+    else if (o.order_state == matching::order::order_status_type::CANCELED_BY_MARKET_ORDER_NOTHING_MATCH)
+    {
+      status = "CANCELED_BY_MARKET_ORDER_NOTHING_MATCH";
+    }
+    else if (o.order_state == matching::order::order_status_type::CANCELED_ALL_BY_IOC)
+    {
+      status = "CANCELED_ALL_BY_IOC";
+    }
+    else if (o.order_state == matching::order::order_status_type::CANCELED_PARTIAL_BY_IOC)
+    {
+      status = "CANCELED_PARTIAL_BY_IOC";
+    }
+    else if (o.order_state == matching::order::order_status_type::CANCELED_BY_FOK)
+    {
+      status = "CANCELED_BY_FOK";
+    }
+    else if (o.order_state == matching::order::order_status_type::CANCELED_BY_MAKER_ONLY)
+    {
+      status = "CANCELED_BY_MAKER_ONLY";
+    }
+    else if (o.order_state == matching::order::order_status_type::REJECT_CANCEL_ORDER_ID_NOT_FOUND)
+    {
+      status = "REJECT_CANCEL_ORDER_ID_NOT_FOUND";
+    }
+    else if (o.order_state == matching::order::order_status_type::REJECT_AMEND_ORDER_ID_NOT_FOUND)
+    {
+      status = "REJECT_AMEND_ORDER_ID_NOT_FOUND";
+    }
+    else if (o.order_state == matching::order::order_status_type::REJECT_DISPLAY_QUANTITY_LARGER_THAN_QUANTITY)
+    {
+      status = "REJECT_DISPLAY_QUANTITY_LARGER_THAN_QUANTITY";
+    }
+    else if (o.order_state == matching::order::order_status_type::REJECT_BUY_STOP_TRIGGER_LARGE_THAN_STOP_LIMITED)
+    {
+      status = "REJECT_BUY_STOP_TRIGGER_LESS_THAN_STOP_LIMITED";
+    }
+    else if (o.order_state == matching::order::order_status_type::REJECT_SELL_STOP_TRIGGER_LESS_THAN_STOP_LIMITED)
+    {
+      status = "REJECT_SELL_STOP_TRIGGER_LESS_THAN_STOP_LIMITED";
+    }
+    else if (o.order_state == matching::order::order_status_type::REJECT_UNKNOW_ORDER_ACTION)
+    {
+      status = "REJECT_UNKNOW_ORDER_ACTION";
+    }
+    else if (o.order_state == matching::order::order_status_type::REJECT_QUANTITY_ZERO)
+    {
+      status = "REJECT_QUANTITY_ZERO";
+    }
+    else
+    {
+      status = "REJECT_LIMITE_ORDER_WITH_MARKET_PRICE";
+    }
+    if (matching::order::order_time_condition::GTC == o.time_condition)
+    {
+      time_condition = "GTC";
+    }
+    else if (matching::order::order_time_condition::IOC == o.time_condition)
+    {
+      time_condition = "IOC";
+    }
+    else if (matching::order::order_time_condition::FOK == o.time_condition)
+    {
+      time_condition = "FOK";
+    }
+    else if (matching::order::order_time_condition::MAKER_ONLY == o.time_condition)
+    {
+      time_condition = "MAKER_ONLY";
+    }
+    else
+    {
+      time_condition = "MAKER_ONLY_REPRICE";
+    }
+    if (matching::order::order_action_type::NEW == o.order_action)
+    {
+      action = "NEW";
+    }
+    else if (matching::order::order_action_type::CANCEL == o.order_action)
+    {
+      action = "CANCEL";
+    }
+    else
+    {
+      action = "AMEND";
+    }
+    if (matching::order::order_matched_type::TAKER == o.matched_type)
+    {
+      matched_type = "TAKER";
+    }
+    else
+    {
+      matched_type = "MAKER";
+    }
   client_to_engine_id_map[o.client_order_id] = o.order_id;
   elog.debug()
       << "action:" << action
@@ -181,6 +185,7 @@ json::Object handle_order(const matching::order& o)
       << std::endl;
 
   json::Object response;
+  response.insert("action", json::String(action));
   response.insert("side", json::String(side));
   response.insert("time_condition", json::String(time_condition));
   response.insert("order_id", json::Integer(o.order_id));
@@ -190,7 +195,6 @@ json::Object handle_order(const matching::order& o)
   response.insert("remain_quantity", json::Integer(o.remain_quantity));
   response.insert("price", json::Integer(o.price));
   response.insert("buy_stop_trigger_price", json::Integer(o.buy_stop_trigger_price));
-
   response.insert("buy_stop_limited_price", json::Integer(o.buy_stop_limited_price));
   response.insert("sell_stop_trigger_price", json::Integer(o.sell_stop_trigger_price));
   response.insert("sell_stop_limited_price", json::Integer(o.sell_stop_limited_price));
