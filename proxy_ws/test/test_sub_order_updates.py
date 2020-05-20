775c9647a66cb3ae254905bbd06a14bb629bb704
Peter Chan <peter.chan@coinflex.com>
2020-05-20 23:09:29 +0800

    add python test script

---
+++ proxy_ws/test/test_sub_order_updates.py
@@ -0,0 +1,22 @@
+import zmq
+
+context = zmq.Context()
+
+# This is where the weather server sits
+frontend = context.socket(zmq.SUB)
+frontend.connect("tcp://127.0.0.1:14001")
+
+# This is our public endpoint for subscribers
+#backend = context.socket(zmq.PUB)
+#backend.bind("tcp://10.1.1.0:8100")
+
+# Subscribe on everything
+frontend.setsockopt(zmq.SUBSCRIBE, b'')
+
+# Shunt messages out to our own subscribers
+while True:
+    # Process all parts of the message
+    #message = frontend.recv_multipart()
+    message = frontend.recv()
+    print(message)
+    #backend.send_multipart(message)
