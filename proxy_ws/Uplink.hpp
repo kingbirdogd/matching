d840fccdf9a9f17231bb9c8fc469bd050cb53bcd
Peter Chan <peter.chan@coinflex.com>
2020-05-21 13:02:44 +0800

    try port 15002

--- proxy_ws/Uplink.hpp
+++ proxy_ws/Uplink.hpp
@@ -38,7 +38,7 @@ namespace proxy {
       : host(host), selector_in(selector_in),
         selector_out(selector_out), num_queue(num_queue), port(matching_port),
         ctx(1), pub_sock(ctx, ZMQ_PUB) {
-      pub_sock.connect("tcp://localhost:14002"); // hard code for testing
+      pub_sock.connect("tcp://localhost:15002"); // hard code for testing
       std::cout << "Connected to xsub" << std::endl;
     }
 
