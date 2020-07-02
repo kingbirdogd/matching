b06edb5c57341663f5d70ea937c477f1d1d56329
Peter Chan <peter.chan@coinflex.com>
2020-07-03 01:04:53 +0800

    use pulsar 2.6.0

--- proxy_md_pulsar/proxy_lws.cpp
+++ proxy_md_pulsar/proxy_lws.cpp
@@ -13,13 +13,15 @@
 #include <netinet/tcp.h>
 #include <iostream>
 #include <thread>
+#include <shared_mutex>
 #include "common/log.h"
 #include "common/scheduler.h"
-#include "proxy_lws_utils.hpp"
+#include "core.h"
+//#include "proxy_lws_utils.hpp"
 //#include "Server.hpp"
 
 #define LWS_PLUGIN_STATIC
-#include "proxy_lws_protocol.hpp"
+//#include "proxy_lws_protocol.hpp"
 
 //using njson = nlohmann::json;
 
@@ -38,7 +40,7 @@ namespace proxy {
 //  static bool skip_auth_allowed = false;
 //  static Scheduler<std::chrono::steady_clock> scheduler;
   std::shared_mutex books_rwlock;
-  std::map<asset_pair_t, Book> books;
+  //std::map<asset_pair_t, Book> books;
 
   bool fee_control_allowed = false;
   bool skip_auth_allowed = false;
@@ -59,66 +61,66 @@ namespace proxy {
 }
 
 
-static struct lws_protocols protocols[] = {
-    //{NULL, NULL, "default", "1"},
-    //{ "http", callback_http_test, sizeof(struct per_session_data__minimal), 256, 0, NULL,0 },
-    LWS_PLUGIN_PROTOCOL_MINIMAL,
-    //{ "http", callback_minimal, 0, 0 },
-    { NULL, NULL, 0, 0 } /* terminator */
-};
-
-static const struct lws_protocol_vhost_options pvo_opt = {
-    NULL,
-    NULL,
-    "default",
-    "1"
-};
-
-static const struct lws_protocol_vhost_options pvo = {
-    NULL,
-    &pvo_opt,
-    "callback_minimal",
-    ""
-};
-
-//static const lws_retry_bo_t retry = {
-//    .secs_since_valid_ping = 3,
-//    .secs_since_valid_hangup = 10,
+//static struct lws_protocols protocols[] = {
+//    //{NULL, NULL, "default", "1"},
+//    //{ "http", callback_http_test, sizeof(struct per_session_data__minimal), 256, 0, NULL,0 },
+//    LWS_PLUGIN_PROTOCOL_MINIMAL,
+//    //{ "http", callback_minimal, 0, 0 },
+//    { NULL, NULL, 0, 0 } /* terminator */
+//};
+//
+//static const struct lws_protocol_vhost_options pvo_opt = {
+//    NULL,
+//    NULL,
+//    "default",
+//    "1"
+//};
+//
+//static const struct lws_protocol_vhost_options pvo = {
+//    NULL,
+//    &pvo_opt,
+//    "callback_minimal",
+//    ""
+//};
+//
+////static const lws_retry_bo_t retry = {
+////    .secs_since_valid_ping = 3,
+////    .secs_since_valid_hangup = 10,
+////};
+//
+//static const lws_retry_bo_t retry = []{
+//    lws_retry_bo_t tmp{};
+//    tmp.secs_since_valid_ping = 3;
+//    tmp.secs_since_valid_hangup = 10;
+//    return tmp;
+//}();
+//
+//static int interrupted;
+//
+//static const struct lws_http_mount mount = {
+//    /* .mount_next */		NULL,		/* linked-list "next" */
+//    /* .mountpoint */		"/",		/* mountpoint URL */
+//    /* .origin */			"./mount-origin",  /* serve from dir */
+//    /* .def */			"index.html",	/* default filename */
+//    /* .protocol */			NULL,
+//    /* .cgienv */			NULL,
+//    /* .extra_mimetypes */		NULL,
+//    /* .interpret */		NULL,
+//    /* .cgi_timeout */		0,
+//    /* .cache_max_age */		0,
+//    /* .auth_mask */		0,
+//    /* .cache_reusable */		0,
+//    /* .cache_revalidate */		0,
+//    /* .cache_intermediaries */	0,
+//    /* .origin_protocol */		LWSMPRO_FILE,	/* files in a dir */
+//    /* .mountpoint_len */		1,		/* char count */
+//    /* .basic_auth_login_file */	NULL,
 //};
 
-static const lws_retry_bo_t retry = []{
-    lws_retry_bo_t tmp{};
-    tmp.secs_since_valid_ping = 3;
-    tmp.secs_since_valid_hangup = 10;
-    return tmp;
-}();
-
-static int interrupted;
-
-static const struct lws_http_mount mount = {
-    /* .mount_next */		NULL,		/* linked-list "next" */
-    /* .mountpoint */		"/",		/* mountpoint URL */
-    /* .origin */			"./mount-origin",  /* serve from dir */
-    /* .def */			"index.html",	/* default filename */
-    /* .protocol */			NULL,
-    /* .cgienv */			NULL,
-    /* .extra_mimetypes */		NULL,
-    /* .interpret */		NULL,
-    /* .cgi_timeout */		0,
-    /* .cache_max_age */		0,
-    /* .auth_mask */		0,
-    /* .cache_reusable */		0,
-    /* .cache_revalidate */		0,
-    /* .cache_intermediaries */	0,
-    /* .origin_protocol */		LWSMPRO_FILE,	/* files in a dir */
-    /* .mountpoint_len */		1,		/* char count */
-    /* .basic_auth_login_file */	NULL,
-};
-
-void sigint_handler(int sig)
-{
-  interrupted = 1;
-}
+//void sigint_handler(int sig)
+//{
+//  interrupted = 1;
+//}
 
 
 
@@ -175,7 +177,7 @@ int main(int argc, const char **argv)
     //proxy::read_inst_config("/tmp/tmp.BejDc4c0An/contrib/nlohmann/instruments.json");
     std::string inst_config_fname = inst_config_option.value_or("");
     std::clog << "Reading instrument config from: " << inst_config_fname << std::endl;
-    proxy::read_inst_config(inst_config_fname);
+    //proxy::read_inst_config(inst_config_fname);
     std::clog << "Finished reading instrument config from: " << inst_config_fname << std::endl;
   }
   if (skip_auth_option) {
@@ -202,7 +204,7 @@ int main(int argc, const char **argv)
   Selector selector_in, selector_out;
   proxy::selector = &selector_in;
   proxy::Uplink uplink(argv[1], selector_in, selector_out, num_queue,
-      matching_port_option.value_or(CORE_PORT),
+      matching_port_option.value_or(core::CORE_PORT),
       pulsar_host_url_option.value(),
       md_pub_snapshot_url_option.value(),
       md_pub_diff_url_option.value(),
@@ -238,12 +240,12 @@ int main(int argc, const char **argv)
   std::thread(&Selectable::pump, std::ref(selector_out)).detach();
   std::thread(&decltype(proxy::scheduler)::run, std::ref(proxy::scheduler)).detach();
 
-  // ==== libwebsocket setup ====
-  struct lws_context_creation_info info;
-  struct lws_context *context;
+//  // ==== libwebsocket setup ====
+//  struct lws_context_creation_info info;
+//  struct lws_context *context;
   const char *p;
   //| LLL_INFO | LLL_PARSER | LLL_HEADER | LLL_EXT | LLL_CLIENT | LLL_DEBUG
-  int n = 0, logs = LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE
+  //int n = 0, logs = LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE
   /* for LLL_ verbosity above NOTICE to be built into lws,
    * lws must have been configured and built with
    * -DCMAKE_BUILD_TYPE=DEBUG instead of =RELEASE */
@@ -251,48 +253,48 @@ int main(int argc, const char **argv)
   /* | LLL_EXT */ /* | LLL_CLIENT */ /* | LLL_LATENCY */
   /* | LLL_DEBUG */;
 
-  signal(SIGINT, sigint_handler);
-
-  if ((p = lws_cmdline_option(argc, argv, "-d")))
-    logs = atoi(p);
-
-  lws_set_log_level(logs, NULL);
-  lwsl_user("LWS minimal ws server | visit http://localhost:7681 (-s = use TLS / https)\n");
-
-  memset(&info, 0, sizeof info); /* otherwise uninitialized garbage */
-  info.port = wss_port_option.value_or(8080);
-  info.mounts = &mount;
-  info.protocols = protocols;
-  //info.pvo = &pvo;
-  info.vhost_name = "localhost";
-  info.ws_ping_pong_interval = 5;
-  info.timeout_secs = 1;
-  info.options =
-      LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
-
-//  if (lws_cmdline_option(argc, argv, "-s")) {
-//    lwsl_user("Server using TLS\n");
-//    info.options |= LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
-//    info.ssl_cert_filepath = "localhost-100y.cert";
-//    info.ssl_private_key_filepath = "localhost-100y.key";
+//  signal(SIGINT, sigint_handler);
+//
+//  if ((p = lws_cmdline_option(argc, argv, "-d")))
+//    logs = atoi(p);
+//
+//  lws_set_log_level(logs, NULL);
+//  lwsl_user("LWS minimal ws server | visit http://localhost:7681 (-s = use TLS / https)\n");
+//
+//  memset(&info, 0, sizeof info); /* otherwise uninitialized garbage */
+//  info.port = wss_port_option.value_or(8080);
+//  info.mounts = &mount;
+//  info.protocols = protocols;
+//  //info.pvo = &pvo;
+//  info.vhost_name = "localhost";
+//  info.ws_ping_pong_interval = 5;
+//  info.timeout_secs = 1;
+//  info.options =
+//      LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
+//
+////  if (lws_cmdline_option(argc, argv, "-s")) {
+////    lwsl_user("Server using TLS\n");
+////    info.options |= LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
+////    info.ssl_cert_filepath = "localhost-100y.cert";
+////    info.ssl_private_key_filepath = "localhost-100y.key";
+////  }
+////
+////  if (lws_cmdline_option(argc, argv, "-h"))
+////    info.options |= LWS_SERVER_OPTION_VHOST_UPG_STRICT_HOST_CHECK;
+////
+////  if (lws_cmdline_option(argc, argv, "-v"))
+////    info.retry_and_idle_policy = &retry;
+//
+//  context = lws_create_context(&info);
+//  if (!context) {
+//    lwsl_err("lws init failed\n");
+//    return 1;
 //  }
 //
-//  if (lws_cmdline_option(argc, argv, "-h"))
-//    info.options |= LWS_SERVER_OPTION_VHOST_UPG_STRICT_HOST_CHECK;
+//  while (n >= 0 && !interrupted)
+//    n = lws_service(context, 1000);
 //
-//  if (lws_cmdline_option(argc, argv, "-v"))
-//    info.retry_and_idle_policy = &retry;
-
-  context = lws_create_context(&info);
-  if (!context) {
-    lwsl_err("lws init failed\n");
-    return 1;
-  }
-
-  while (n >= 0 && !interrupted)
-    n = lws_service(context, 1000);
-
-  lws_context_destroy(context);
+//  lws_context_destroy(context);
 
   return 0;
 }
